2ee02242ea6e9336b7260833564874a7b521493d
codingshot <45281667+codingshot@users.noreply.github.com>
2023-05-20 21:55:08 -0400

    not working but trying
    - showing borsch error

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# pizza
-Pizza DAO NFT Poap for global pizza day with onboard
+# PizzaDAO Keypom Onboard
+'''
+npm install
+'''
+
+
+Switch to mainnet in cli
+export NEAR_ENV=mainnet
+'''
+near login 
+'''
+after switching to near mainnet // add how to switch to mainnet
+after you near login replace the funder account in this case onboarder.near with your account
+put telegram api key in .env and enter your telegram chatid and add your telegram bot
+add .env and node-modules folder in .gitignore
+node grifters.ts {number-of-keypom-drops}
+
+
+# ToDo
+- add QR code generator for chat
+- create north africa dao
+- create nft mint for pizza dao
+- 
+
+
+# DAO
+pizza.sputnik-dao.near
+POAP link 
+https://ipfs.near.social/ipfs/bafkreigjhig32jinjqeje4jva5ygki5345rfzhyg7vksbhlvwoiaw7ew3e
+
---
+++ pizza.ts
@@ -0,0 +1,206 @@
+async function createDAODrop() {
+    const path = require("path");
+const homedir = require("os").homedir();
+const { KeyPair, keyStores, connect, Account } = require("near-api-js");
+var assert = require('assert');
+require('dotenv').config();
+let telegram_public_bot = process.env.TELEGRAM_KEYBOT;
+
+const keypom = require("keypom-js");
+// const { DEV_CONTRACT } = require("./configurations");
+// const TESTNET_DAO_CONTRACT = "onboard.sputnikv2.testnet";
+// let chatID = process.env.chatID;
+let chatID = "-1001916907491" // change this link into pizza dao
+// generate telegram id link for pizza onboard
+// qr code generator
+// const QRCode = require('qrcode-generator');
+
+// const url = 'https://example.com';
+// const qr = QRCode(0, 'M');
+// qr.addData(url);
+// qr.make();
+// const qrCodeSvg = qr.createSvgTag(4);
+
+// Do something with the generated QR code SVG string
+// add number of drops to CLI
+// Retrieve the command-line arguments using the process.argv array
+const args = process.argv.slice(2);
+
+// Check if the argument was passed
+if (args.length === 0) {
+  console.error('Please provide a number as an argument.');
+  process.exit(1);
+}
+
+// Get the argument value and convert it to a number
+const numberOfKeys = parseInt(args[0], 10);
+if (isNaN(numberOfKeys)) {
+  console.error('Invalid number provided as an argument.');
+  process.exit(1);
+}
+// Use the number in your script
+console.log(`The number of keys to generate is: ${numberOfKeys}.`);
+const TelegramBot = require('node-telegram-bot-api');
+const bot = new TelegramBot(telegram_public_bot, { polling: false });
+
+
+const {
+	initKeypom,
+	getEnv,
+	createDrop,
+    parseNearAmount,
+    createNFTSeries,
+    formatLinkdropUrl
+} = keypom
+
+// Change this to your account ID
+const FUNDER_ACCOUNT_ID = "rarepizzas.near"; // change to rare pizzas
+const NETWORK_ID = "mainnet";
+const NFT_TOKEN_ID = "keypom-token-" + Date.now().toString();
+const CREDENTIALS_DIR = ".near-credentials";     // Initiate connection to the NEAR blockchain.
+const credentialsPath =  path.join(homedir, CREDENTIALS_DIR);
+const numberOfKeys1 = 1; // basically number of links to generate // change back
+let keyStore = new keyStores.UnencryptedFileSystemKeyStore(credentialsPath);  
+
+let nearConfig = {
+    networkId: NETWORK_ID,
+    keyStore: keyStore,
+    nodeUrl: `https://rpc.${NETWORK_ID}.near.org`,
+    walletUrl: `https://wallet.${NETWORK_ID}.near.org`,
+    helperUrl: `https://helper.${NETWORK_ID}.near.org`,
+    explorerUrl: `https://explorer.${NETWORK_ID}.near.org`,
+};  
+
+    
+let near = await connect(nearConfig);
+const fundingAccount = new Account(near.connection, FUNDER_ACCOUNT_ID)
+// If a NEAR connection is not passed in and is not already running, initKeypom will create a new connection
+// Here we are connecting to the testnet network
+await initKeypom({
+    near,
+    network: NETWORK_ID,
+});
+
+// Create drop with 10 keys and 2 key uses each
+let {keys, dropId} = await createDrop({
+    account: fundingAccount,
+    numKeys: numberOfKeys,
+    config: {
+        usesPerKey: 1
+    },
+            depositPerUseNEAR: "0.01",
+
+    fcData: {
+        methods: [
+            [
+                // {
+                //     receiverId: "nft-v2.keypom.near", // check if this
+                //     methodName: "nft_mint",
+                //     args: JSON.stringify({
+                //         // Change this token_id if it already exists -> check explorer transaction
+                //         token_id: NFT_TOKEN_ID,
+                //         metadata: {
+                //             title: "Global Pizza Day POAP 2023",
+                //             description: "PizzaDAO comes to North Africa. With this NFT you are automatically onboarded on-chain to North Africa DAO, Onboard DAO, and Pizza DAO on NEAR.",
+                //             media: "https://ipfs.near.social/ipfs/bafkreigjhig32jinjqeje4jva5ygki5345rfzhyg7vksbhlvwoiaw7ew3e",
+                //         }
+                //     }),
+                //     accountIdField: "receiver_id",
+                //     // Attached deposit of 1 $NEAR for when the receiver makes this function call
+                //     attachedDeposit: parseNearAmount("0.1")  // give less and see what happens
+                // },
+                {
+                    receiverId: "onboarddao.sputnik-dao.near",
+                    methodName: "add_proposal",
+                    args: JSON.stringify(
+                        {
+                            "proposal": {
+                            "description": "Welcome to Onboard DAO. Thanks for coming to Global Pizza Day Tangier 🍕🌍",
+                            "kind": {
+                                "AddMemberToRole": {
+                                "role": "Onboardees"
+
+                                }
+                            }
+                            }
+                        }
+                    ),
+                    accountIdField: "proposal.kind.AddMemberToRole.member_id",
+                    funderIdField: "funder",
+                    attachedDeposit: parseNearAmount("0.01")
+                }
+                // {
+                //     receiverId: "africa.sputnik-dao.near",
+                //     methodName: "add_proposal",
+                //     args: JSON.stringify(
+                //         {
+                //             "proposal": {
+                //             "description": "Welcome to North Africa Blockchain. Thanks for coming to Global Pizza Day Tangier 🍕🌍",
+                //             "kind": {
+                //                 "AddMemberToRole": {
+                //                 "role": "members"
+
+                //                 }
+                //             }
+                //             }
+                //         }
+                //     ),
+                //     accountIdField: "proposal.kind.AddMemberToRole.member_id",
+                //     funderIdField: "funder",
+                //     attachedDeposit: parseNearAmount("0.01")
+                // },
+                // {
+                //     receiverId: "pizza.sputnik-dao.near",
+                //     methodName: "add_proposal",
+                //     args: JSON.stringify(
+                //         {
+                //             "proposal": {
+                //             "description": "Welcome to Pizza DAO on NEAR. Thanks for coming to Global Pizza Day Tangier 🍕🌍",
+                //             "kind": {
+                //                 "AddMemberToRole": {
+                //                 "role": "Pizza Trainee"
+
+                //                 }
+                //             }
+                //             }
+                //         }
+                //     ),
+                //     accountIdField: "proposal.kind.AddMemberToRole.member_id",
+                //     funderIdField: "funder",
+                //     attachedDeposit: parseNearAmount("0.01")
+                // }
+
+            ],
+        ] 
+            
+    }   
+})
+
+    const {contractId: KEYPOM_CONTRACT} = getEnv()
+    let membership = formatLinkdropUrl({
+        customURL: "https://wallet.near.org/linkdrop/CONTRACT_ID/SECRET_KEY",
+        secretKeys: keys.secretKeys,
+        contractId: KEYPOM_CONTRACT,
+    })
+    console.log(`
+    
+    For NEW Grifter Members: 
+    
+    ${membership}
+    
+
+    `)
+    let output = "You are now part of PizzaDAO, North Africa Blockchain, and OnboardDAO and received a POAP for Global Pizza Day 2023"+ JSON.stringify(membership);
+
+      bot.sendMessage(chatID, output);
+
+
+
+    return keys
+}
+
+createDAODrop()
+
+module.exports = {
+    createDAODrop
+}
